a2fce40d681306ca700292fb0b2990a54f7a6130
Rick Peyton <rick.peyton@daveramsey.com>
2018-12-30 08:25:56 -0600

    Use Sale Price if Sale Price present

--- lib/nintendo_eshop/parser.rb
+++ lib/nintendo_eshop/parser.rb
@@ -3,10 +3,12 @@ module NintendoEshop
     def self.games_list(list)
       current_time = Time.now.to_i
       list.dig("games", "game").map do |game|
+        sale_price = game["sale_price"]
+        price = sale_price.nil? ? game["eshop_price"] : sale_price
         NintendoEshop::Game.new(
           nsid: game["nsuid"],
           title: game["title"],
-          price: (game["eshop_price"].to_f * 100).to_i,
+          price: (price.to_f * 100).to_i,
           image: game["front_box_art"],
           created_at: current_time
         )
