54613af47a05d7b71a17b458c1f7840f3b7aff93
Rick Peyton <rick.peyton@daveramsey.com>
2018-12-29 21:30:40 -0600

    Removing commented out cookie line

--- lib/nintendo_eshop/http_client.rb
+++ lib/nintendo_eshop/http_client.rb
@@ -9,7 +9,6 @@ module NintendoEshop
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_PEER
       req = set_method(method).new(uri)
-      # req.add_field "Cookie", "NINTENDO=10.24.11.35.1546133465366875; JSESSIONID=7FE7141126A677298E77647F02FA0960; nintendo.locale.country=US; nintendo.locale.language=en"
       res = http.request(req)
       JSON.parse(res.body)
     rescue StandardError => e
